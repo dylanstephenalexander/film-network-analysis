884f6fb46154f8390a2bfa0bf6508c159db9f695
dylanstephenalexander <dylanstephenalexander@gmail.com>
2025-11-25 15:10:09 -0800

    fix dataset import

--- Code/research_question_3.r
+++ Code/research_question_3.r
@@ -6,7 +6,7 @@ library(dplyr)
 # import datasets
 imdb_names <- fread("~/datasets/imdb_dataset/names.csv")
 imdb_titles <- fread("~/datasets/imdb_dataset/titles.csv")
-tmdb <- fread("datasets/tmdb_dataset/tmdb_dataset.csv")
+tmdb <- fread("~/datasets/tmdb_dataset/tmdb_dataset.csv")
 
 colnames(imdb_names)
 colnames(imdb_titles)
