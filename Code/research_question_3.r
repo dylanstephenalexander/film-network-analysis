8246f7b69d16db3b5b7dc2abc43e3976f9dc087c
dylanstephenalexander <dylanstephenalexander@gmail.com>
2025-11-27 10:57:01 -0800

    moved dataset to fix weird behaviour
    update .gitignore
    
    moved dataset to fix weird behaviour

--- Code/research_question_3.r
+++ Code/research_question_3.r
@@ -4,9 +4,9 @@ library(data.table)
 library(dplyr)
 
 # import datasets
-imdb_names <- fread("~/datasets/imdb_dataset/names.csv")
-imdb_titles <- fread("~/datasets/imdb_dataset/titles.csv")
-tmdb <- fread("~/datasets/tmdb_dataset/tmdb_dataset.csv")
+imdb_names <- fread('/Users/dylanalexander/Desktop/term 1 2025-26/cosc 421/datasets/imdb_dataset/names.csv')
+imdb_titles <- fread('/Users/dylanalexander/Desktop/term 1 2025-26/cosc 421/datasets/imdb_dataset/titles.csv')
+tmdb <- fread('/Users/dylanalexander/Desktop/term 1 2025-26/cosc 421/datasets/tmdb_dataset/tmdb_dataset.csv')
 
 colnames(imdb_names)
 colnames(imdb_titles)
