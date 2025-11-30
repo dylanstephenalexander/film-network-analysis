903a0e4af4d24fdb3bdb45cfd4ec70912c49f027
dylanstephenalexander <dylanstephenalexander@gmail.com>
2025-11-29 17:08:00 -0800

    defined filters and cleaned up dataset

--- Code/research_question_3.r
+++ Code/research_question_3.r
@@ -12,3 +12,38 @@ colnames(imdb_names)
 colnames(imdb_titles)
 colnames(tmdb)
 
+# extrapolating release_year field from release_date
+
+tmdb <- tmdb %>%
+  mutate(release_year = as.numeric(substr(release_date, 1, 4)))
+
+# cleaning up dataset
+# minimum budget set at $1M, minimum release_year set at 2000, qualifying movies must have budget, revenue, release_year data
+
+tmdb_cleaned <- tmdb %>%
+  filter(
+    budget >= 1000000,
+    release_year >= 2000,
+    !is.na(budget),
+    !is.na(revenue),
+    !is.na(release_year)
+  )
+
+# calculating ROI field
+
+tmdb_cleaned <- tmdb_cleaned %>%
+  mutate(roi_percent = ((revenue - budget) / budget) * 100)
+
+
+### FOR REFERENCE ### 
+
+num_moves <- nrow(tmdb_cleaned) # num movies = 11832 
+min_roi_percent <- min(tmdb_cleaned$roi_percent) # max_roi_percent = 49900%
+max_roi_percent <- max(tmdb_cleaned$roi_percent) # max_roi_percent = -100%
+mean_roi_percent <- mean(tmdb_cleaned$roi_percent) # mean_roi_percent = 68.4601%
+
+# 'flop' defined as negative ROI
+num_flops <- sum(tmdb_cleaned$roi_percent < 0) # num_flops = 7591
+# 'success' defined as positive ROI
+num_successes <- sum(tmdb_cleaned$roi_percent > 0) # num_successes = 4223
+
